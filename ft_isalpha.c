8a2d3f42ea52d80b0467329a9e7100cb8fb784d4
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-22 19:39:57 -0300

    Formata norma.

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 19:14:56 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/22 19:35:51 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/22 19:39:25 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,3 @@ int	ft_isalpha(char c)
 	else
 		return (0);
 }
-
-/*#include <unistd.h>
-
-int	main(void)
-{
-	char	*str_a = "t.este";
-	char	*str_b = "a";
-//	int	res;
-	char	c_res;
-
-	c_res = ft_str_is_alpha(str_a) + '0';
-	write(1, &c_res, 1);
-	c_res = ft_str_is_alpha(str_b) + '0';
-        write(1, &c_res, 1);
-}*/
