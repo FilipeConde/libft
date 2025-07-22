4f43e58b4dac9f9aaba94b9234f2003dad496bd7
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-22 20:18:54 -0300

    Comenta arquivos com definições duplicadas de strlen().

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,45 +6,45 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 11:13:27 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/20 19:46:11 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/22 20:14:30 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+// #include <stdlib.h>
 
-int	ft_strlen(char *str)
-{
-	int	i;
+// int	ft_strlen(char *str)
+// {
+// 	int	i;
 
-	i = 0;
-	while (*str)
-	{
-		str++;
-		i++;
-	}
-	return (i);
-}
+// 	i = 0;
+// 	while (*str)
+// 	{
+// 		str++;
+// 		i++;
+// 	}
+// 	return (i);
+// }
 
-char	*ft_strdup(char *src)
-{
-	char	*result;
-	int		i;
+// char	*ft_strdup(char *src)
+// {
+// 	char	*result;
+// 	int		i;
 
-	i = 0;
-	result = (char *) malloc(ft_strlen(src) * sizeof(char));
-	if (src[0] == '\0')
-	{
-		result[0] = '\0';
-		return (result);
-	}
-	while (src[i] != '\0')
-	{
-		result[i] = src[i];
-		i++;
-	}
-	result[i] = '\0';
-	return (result);
-}
+// 	i = 0;
+// 	result = (char *) malloc(ft_strlen(src) * sizeof(char));
+// 	if (src[0] == '\0')
+// 	{
+// 		result[0] = '\0';
+// 		return (result);
+// 	}
+// 	while (src[i] != '\0')
+// 	{
+// 		result[i] = src[i];
+// 		i++;
+// 	}
+// 	result[i] = '\0';
+// 	return (result);
+// }
 /*
 #include <stdio.h>
 #include <string.h>
