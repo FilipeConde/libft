6220278a97ba49e04d31d22d63126f77a29d0b57
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-23 22:40:51 -0300

    Refatora ft_strlcat().

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,59 +1,38 @@
-// int	ft_strlen(char *str)
-// {
-// 	int	i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 22:18:57 by fconde-p          #+#    #+#             */
+/*   Updated: 2025/07/23 22:38:57 by fconde-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-// 	i = 0;
-// 	while (str[i] != '\0')
-// 	{
-// 		i++;
-// 	}
-// 	return (i);
-// }
+#include "libft.h"
 
-// unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
-// {
-// 	unsigned int	i;
-// 	unsigned int	j;
-// 	unsigned int	expected_len;
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	expected_len;
 
-// 	i = 0;
-// 	j = 0;
-// 	expected_len = ft_strlen(dest) + ft_strlen(src);
-// 	if (size == 0)
-// 		return (ft_strlen(src));
-// 	if(ft_strlen(dest) >= (int) size)
-// 		return (ft_strlen(src) + size);
-// 	while (dest[i] != '\0')
-// 	{
-// 		i++;
-// 	}
-// 	while (src[j] != '\0' && i < size - 1)
-// 	{
-// 		dest[i++] = src[j++];
-// 	}
-// 	dest[i++] = '\0';
-
-// 	return (expected_len);
-// }
-
-
-// #include <bsd/string.h>
-// #include <stdio.h>
-// int	main(void)
-// {
-// 	char	dest1[12] = "01234";
-// 	char	dest2[12] = "01234";
-// 	char	*src = "abcde";
-// 	int	size = 0;
-// 	int	rest1;
-// 	int	rest2;
-
-// 	rest1 = strlcat(dest1, src, size);
-// 	printf("%s\n", dest1);
-// 	printf("%d\n", rest1);
-// 	//printf("%d\n", ft_strlen("0123"));
-// 	rest2 = ft_strlcat(dest2, src, size);
-// 	printf("%s\n", dest2);
-// 	printf("%d\n", rest2);
-
-// }
+	i = 0;
+	j = 0;
+	expected_len = ft_strlen(dst) + ft_strlen(src);
+	if (size == 0)
+		return (ft_strlen(src));
+	if (ft_strlen(dst) >= size)
+		return (ft_strlen(src) + size);
+	while (dst[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && i < size - 1)
+	{
+		dst[i++] = src[j++];
+	}
+	dst[i++] = '\0';
+	return (expected_len);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:30:31 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/23 20:50:02 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/23 22:35:37 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int				ft_isdigit(int c);
 int				ft_isprint(int c);
 // int				ft_strcmp(const char *s1, const char *s2);
 char			*ft_strdup(char *src);
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+size_t			ft_strlcat(char *dst, const char *src, size_t size);
 size_t			ft_strlcpy(char *dst, const char *src, size_t size);
 size_t			ft_strlen(const char *s);
 int				ft_tolower(int c);
