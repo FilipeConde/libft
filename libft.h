ea4dd03cdfac8076188b1f7f4a862225bf9c30e6
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-24 23:09:36 -0300

    Implementa ft_bzero().

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:30:31 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/24 21:13:14 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/24 22:35:40 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,6 @@ int				ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_toupper(int c);
 int				ft_isalnum(int c);
 int				ft_isascii(int c);
+void			ft_bzero(void *s, size_t n);
 
 #endif
