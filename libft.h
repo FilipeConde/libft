b601d88990e7b95a4f7c28e4c8e79c0782639245
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-24 21:01:33 -0300

    Implementa ft_isalnum().

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:30:31 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/23 22:45:17 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/24 20:51:35 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ size_t			ft_strlen(const char *s);
 int				ft_tolower(int c);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_toupper(int c);
+int				ft_isalnum(int c);
 
 #endif
