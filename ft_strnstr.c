2e299ca3cc1d54559356c4b1ae43ee1d004792f9
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-26 17:28:56 -0300

    Refina método para cobrir requisitos definidos na suíte de teste automatizado.

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 21:15:56 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/26 16:05:32 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/26 17:26:04 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	int	i;
+	size_t	i;
 
 	if (little[0] == '\0')
 		return ((char *)big);
 	i = 0;
 	if (ft_strlen(little) > ft_strlen(big) || len == 0)
 		return (NULL);
-	while (big[i])
+	if (len == 1)
 	{
-		while (*little)
-		{
-			if (ft_strncmp(big, little, len) == 0)
-				return ((char *)&big[i]);
-			little++;
-		}
+		if (big[0] == *little)
+			return (((char *)&big[i]));
+		else
+			return (NULL);
+	}
+	while (big[i] != '\0' && i < len - 1)
+	{
+		if (ft_strncmp(&big[i], little, ft_strlen(little)) == 0)
+			return ((char *)&big[i]);
 		i++;
 	}
 	return (NULL);
