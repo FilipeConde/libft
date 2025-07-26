d389c63ce8f4728758d118ab0a35224e965aa760
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-26 19:55:46 -0300

    Implementa ft_memchr().

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:30:31 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/26 17:58:42 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/26 18:37:43 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,6 @@ char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
 char			*ft_strnstr(const char *big, const char *little, size_t len);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
+void			*ft_memchr(const void *s, int c, size_t n);
+
 #endif
