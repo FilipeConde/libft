11b1c4bda60d521e4e6182a4740b1f2da7e74f4e
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-27 16:34:34 -0300

    Ajusta ft_atoi() para atender aos comportamentos de atoi oficial.

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 20:02:19 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/26 21:00:57 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/27 16:33:23 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,27 @@ int	ft_atoi(const char *nptr)
 {
 	int	sign;
 	int	acc;
-	// int	i;
 
 	sign = 1;
 	acc = 0;
-	// i = 0;
-	while (*nptr == ' ' || (*nptr >= 9 && *nptr <= 13) || *nptr == '+')
+
+	// ignore caracters
+	while (*nptr >= 0 && *nptr <= 32)
+		nptr++;
+	if (ft_isalpha(*nptr))
+		return (0);
+	if (*nptr == '-' || *nptr == '+')
 	{
+		if (ft_isdigit(*(nptr + 1)) == 0)
+			return  (0);
+		if (*nptr == '-')
+			sign = -1;
 		nptr++;
 	}
 	while (*nptr)
 	{
-		if (*nptr == '-')
-		{
-			sign = -1;
-		}
+		if ((sign == -1) && ft_isdigit(*nptr) == 0)
+			return (0);
 		if (ft_isdigit(*nptr) > 0)
 		{
 			while (ft_isdigit(*nptr) > 0)
@@ -44,18 +50,3 @@ int	ft_atoi(const char *nptr)
 	}
 	return (acc * sign);
 }
-
-// int	ft_atoi(const char *nptr)
-// {
-// 	int	i_nptr;
-// 	int	i;
-
-// 	i = 0;
-// 	i_nptr = 0;
-// 	while (nptr[i] != '\0')
-// 	{
-// 		i_nptr = i_nptr * 10 + (nptr[i] - '0');
-// 		i++;
-// 	}
-// 	return (i_nptr);
-// }
