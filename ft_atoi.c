6fe6d7b14dcf5a28b5325caada991734f9bf1de6
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-27 18:52:37 -0300

    Cria função check_sign().

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 20:02:19 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/27 16:49:26 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/27 18:52:14 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,34 @@ static int	gen_int(char *ptr, int acc)
 	return (acc);
 }
 
+static int	check_sign(char *ptr, int *sign)
+{
+	if (*ptr == '-' || *ptr == '+')
+	{
+		if (ft_isdigit(*(ptr + 1)) == 0)
+			return (0);
+		if (*ptr == '-')
+			*sign = -1;
+		ptr++;
+	}
+	return (1);
+}
+
 int	ft_atoi(const char *nptr)
 {
 	int	sign;
 	int	acc;
+	int	*ptr_sign;
 
+	ptr_sign = &sign;
 	sign = 1;
 	acc = 0;
 	while (*nptr >= 0 && *nptr <= 32)
 		nptr++;
 	if (ft_isalpha(*nptr))
 		return (0);
-	if (*nptr == '-' || *nptr == '+')
-	{
-		if (ft_isdigit(*(nptr + 1)) == 0)
-			return (0);
-		if (*nptr == '-')
-			sign = -1;
-		nptr++;
-	}
+	if (!check_sign((char *)nptr, ptr_sign))
+		return (0);
 	while (*nptr)
 	{
 		if (ft_isdigit(*nptr) > 0)
