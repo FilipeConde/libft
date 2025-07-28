a955dea9ae874f5c324217104a07a3ac043f6c68
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-28 17:04:50 -0300

    Ajusta identação segundo norma.

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:30:31 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/27 19:43:20 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/28 17:04:06 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ char			*ft_strnstr(const char *big, const char *little, size_t len);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_atoi(const char *nptr);
-void            *ft_calloc(size_t nmemb, size_t size);
+void			*ft_calloc(size_t nmemb, size_t size);
+
 
 #endif
