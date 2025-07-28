e1b90f14ff5b6faff0ecf765fd63c35833c213c5
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-28 19:39:24 -0300

    Implmenta otimização de alocação de memória.

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 17:16:17 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/28 18:48:29 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/28 19:38:37 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	i;
+	size_t	s_len;
+	size_t	sub_len;
 
-	if (start > (ft_strlen(s) - 1))
+	i = 0;
+	s_len = ft_strlen(s);
+	if (start > (s_len - 1))
 	{
 		substr = malloc(1);
 		if (!substr)
@@ -25,44 +29,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		ft_memset(substr, 0, 1);
 		return (substr);
 	}
-	i = 0;
-	substr = (char *)malloc((len + 1) * sizeof(char));
-	while (s[start] != '\0' && i < len)
+	if (len > (s_len - start))
+		sub_len = s_len - start;
+	else
+		sub_len = len;
+	substr = (char *)malloc((sub_len + 1) * sizeof(char));
+	while (s[start + i] != '\0' && i < len)
 	{
-		substr[i] = s[start];
+		substr[i] = s[start + i];
 		i++;
-		start++;
 	}
-	substr[i] = '\0';
+	substr[sub_len] = '\0';
 	return (substr);
 }
-
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	char	str[] = "Foi o melhor dos tempos, foi o pior dos tempos...";
-
-	// CT: Extrai do início
-	printf("CT01: %s\n", ft_substr(str, 0, 3));
-	
-	// CT: Extrai do meio
-	printf("CT02: %s\n", ft_substr(str, 6, 17));
-	
-	// CT: Inicia fora dos limites da str base
-	printf("CT03: %s\n", ft_substr(str, 100, 10));
-	
-	// CT: Extrai tamanho 0
-	printf("CT04: %s\n", ft_substr(str, 4, 0));
-
-	// CT: Extrai tamanho 0
-	printf("CT05: %s\n", ft_substr(str, 0, 0));
-
-	// CT: Extrai tamanho acima do existente na base
-	printf("CT06: %s\n", ft_substr(str, 0, 70));
-
-	// CT: 
-	// printf("CT0x: %s\n", ft_substr(str, 0, 0));
-}
-	*/
