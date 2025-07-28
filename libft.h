23a0547050b406666d58347fb3c253dcb080d186
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-28 20:15:11 -0300

    Estabelece base da função.

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:30:31 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/28 17:15:11 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/28 20:14:53 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_atoi(const char *nptr);
 void			*ft_calloc(size_t nmemb, size_t size);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
 
 #endif
