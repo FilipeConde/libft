bfec174ecceb18b98eb5d7fc469e512a4a3b6ef2
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-28 21:13:12 -0300

    Aplica calloc para inicializar ponteiro ao alocar memória.

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 20:12:31 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/28 20:34:32 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/28 21:08:47 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s1_size;
 	size_t	s2_size;
 
-	s1_size = 0;
-	s2_size = 0;
 	s1_size = ft_strlen(s1);
 	s2_size = ft_strlen(s2);
 	if ((s1_size == 0 && s2_size == 0))
@@ -30,21 +28,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		ft_memset(ptr, 0, 1);
 		return (ptr);
 	}
-	ptr = malloc((s1_size + s2_size + 1) * sizeof(char));
-	if(!ptr)
+	ptr = ft_calloc((s1_size + s2_size + 1), sizeof(char));
+	if (!ptr)
 		return (NULL);
-	ft_memcpy(ptr, s1, s1_size);
-	ft_memcpy(ptr + s1_size, s2, s2_size);
-	ptr[s1_size + s2_size + 1] = '\0';
+	ft_strlcat(ptr, s1, s1_size + 1);
+	ft_strlcat(ptr, s2, s1_size + s2_size + 1);
 	return (ptr);
 }
-
-// #include <stdio.h>
-
-// int	main(void)
-// {
-// 	char s1[] = "teste123";
-// 	char s2[] = "teste123";
-
-// 	printf("%s", ft_strjoin(s1, s2));
-// }
