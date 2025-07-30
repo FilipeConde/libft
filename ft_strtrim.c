82f4ce94298c9edc9f0c0bbc115e78a1b3c8b1f4
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-30 18:40:37 -0300

    Implementa check_start().

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,25 +6,28 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 18:26:08 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/29 18:56:14 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/30 18:39:57 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#include <stdio.h>
+
 static size_t	check_start(char const *src, char const *set)
 {
-	// size_t	index;
 	size_t	i;
 	size_t	j;
 
 	i = 0;
 	while (src[0 + i] == set[0])
-	// if (src[0 + i] == set[0])
 	{
 		while (src[i] == set[j])
-		i++;
-		j++;
+		{
+			i++;
+			j++;
+		}
+		j = 0;
 	}
 	return (i);
 }
@@ -35,7 +38,28 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	size_s1;
 	size_t	start;
 	size_t	end;
+	size_t	i;
 
-	size_s1 = ft_strlen(*s1);
-	check_start(s1, set);
+	i = 0;
+	size_s1 = ft_strlen(s1);
+	start = check_start(s1, set);
+	printf("%zu", start);
+	ptr = (char *)malloc(size_s1 * sizeof(char));
+	while (*ptr)
+	{
+		ptr[i] = s1[start + i];
+		i++;
+	}
+	
+	return (ptr);
+}
+
+
+int	main(void)
+{
+	char	str[] = "1212teste12";
+	char	*res;
+
+	res = ft_strtrim(str, "12");
+	printf("%s", res);
 }
