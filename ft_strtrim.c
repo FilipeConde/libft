b5af39d22f75fb80cd23b807f8577819a814a3c2
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-31 18:21:03 -0300

    Implementa comportamento com bloco de 'set' em ordem fixa específica.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 18:26:08 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/30 18:39:57 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/30 23:35:26 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ static size_t	check_start(char const *src, char const *set)
 	size_t	j;
 
 	i = 0;
+	j = 0;
 	while (src[0 + i] == set[0])
 	{
 		while (src[i] == set[j])
@@ -32,34 +33,55 @@ static size_t	check_start(char const *src, char const *set)
 	return (i);
 }
 
+static size_t	check_end(char const *src, char const *set)
+{
+	size_t	src_count;
+	size_t	set_count;
+
+	src_count = ft_strlen(src) - 1;
+	set_count = ft_strlen(set) - 1;
+	while (src[src_count] == set[set_count])
+	{
+		while (src[src_count] == set[set_count])
+		{
+			src_count--;
+			if (src_count == 0 || set_count == 0)
+			{
+				break;
+			}
+			set_count--;
+		}		
+		set_count = ft_strlen(set) - 1;
+	}
+	return (src_count);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*ptr;
 	size_t	size_s1;
 	size_t	start;
 	size_t	end;
-	size_t	i;
 
-	i = 0;
-	size_s1 = ft_strlen(s1);
+	if (!s1 || !set)
+		return (NULL);
 	start = check_start(s1, set);
-	printf("%zu", start);
-	ptr = (char *)malloc(size_s1 * sizeof(char));
-	while (*ptr)
-	{
-		ptr[i] = s1[start + i];
-		i++;
-	}
-	
+	end = check_end(s1, set);
+	size_s1 = end - start + 1;
+
+	ptr = (char *)ft_calloc((size_s1 + 1), sizeof(char));
+	if (!ptr)
+		return (NULL);
+	ft_memcpy(ptr, s1 + start, size_s1 + 1);	
 	return (ptr);
 }
 
 
 int	main(void)
 {
-	char	str[] = "1212teste12";
+	char	str[] = "   xxxtripouille";
 	char	*res;
 
-	res = ft_strtrim(str, "12");
-	printf("%s", res);
+	res = ft_strtrim(str, " x");
+	printf("RESULTADO: %s\n", res);
 }
