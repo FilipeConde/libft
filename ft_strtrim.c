ff93cf12dcb324db256715d5820dbc338ddab58f
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-07-31 21:25:28 -0300

    Implementa nova lógica para ft_strtrim().

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 18:26:08 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/31 18:55:12 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/07/31 21:24:51 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,73 +14,26 @@
 
 #include <stdio.h>
 
-static size_t	check_start(char const *src, char const *set)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	while (src[i] != '\0' && set[j] != '\0')
-	{
-		if (src[i] == set[j])
-		{
-			i++;
-			j = 0;
-		}
-		else
-			j++;
-	}
-	return (i);
-}
-
-static size_t	check_end(char const *src, char const *set)
-{
-	size_t	src_count;
-	size_t	set_count;
-
-	src_count = ft_strlen(src) - 1;
-	set_count = 0;
-	while (src_count != 0 && set[set_count] != '\0')
-	{
-		if (src[src_count] == set[set_count])
-		{
-			src_count--;
-			set_count = 0;
-		}
-		else
-			set_count++;
-	}
-	return (src_count);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*ptr;
-	size_t	size_s1;
 	size_t	start;
 	size_t	end;
 
-	if (!s1 || !set)
-		return (NULL);
-	start = check_start(s1, set);
-	end = check_end(s1, set);
-	if (start > end)
-		return (NULL);
-	size_s1 = end - start + 1;
-	ptr = (char *)ft_calloc((size_s1 + 1), sizeof(char));
-	if (!ptr)
-		return (NULL);
-	ft_memcpy(ptr, s1 + start, size_s1 + 1);	
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	if (end <= start)
+	{
+		ptr = ft_calloc(1, sizeof(char));
+		if (!ptr)
+			return (NULL);
+		return (ptr);
+	}
+	ptr = ft_calloc((end - start + 1), sizeof(char));
+	ft_strlcpy(ptr, (s1 + start), (end - start + 1));
 	return (ptr);
-}
-
-
-int	main(void)
-{
-	char	str[] = "  x xx xtripouillex x  xx x";
-	char	*res;
-
-	res = ft_strtrim(str, "t x");
-	printf("RESULTADO: %s\n", res);
 }
