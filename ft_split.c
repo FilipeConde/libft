eb81115bfc24eab78006279712f613737a5e57de
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-02 16:27:30 -0300

    Estabelece lógica de cópia (não pega o último elemento).

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 17:56:46 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/01 21:32:05 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/02 16:25:48 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,46 +35,66 @@ static int	count_elements(char const *s, char c)
 	return (index);
 }
 
-static unsigned int	check_delimiter(char const *s, char c)
-{
-	unsigned int	index;
+// static unsigned int	check_delimiter(char const *s, char c)
+// {
+// 	unsigned int	index;
 
-	index = 0;
-	while (*s != c && *s != '\0')
-	{
-		if (*s == c)
-			return (index);
-		index++;
-		s++;
-	}
-	return (index);
-}
+// 	index = 0;
+// 	while (*s != c && *s != '\0')
+// 	{
+// 		if (*s == c)
+// 			return (index);
+// 		index++;
+// 		s++;
+// 	}
+// 	return (index);
+// }
+
+// static unsigned int	get_next_valid_index(char const *s, char c)
+// {
+// 	unsigned int	index;
+
+// 	index = 0;
+// 	while (*s != '\0')
+// 	{
+// 		if (*s == c)
+// 			return (index);
+// 		index++;
+// 		s++;
+// 	}
+// 	return (index);
+// }
 
 char	**ft_split(char const *s, char c)
 {
 	char			**splited_s;
 	unsigned int	start_i;
-	unsigned int	end_i;
 	int				i;
+	int				j;
 	char			*ptr_s;
 
 	ptr_s = ft_strtrim(s, &c);
-	// printf("TESTE: %s\n\n", ptr_s);
-	splited_s = malloc((count_elements(s, c) + 1) * sizeof(char *));
-	if (!**splited_s)
+	splited_s = malloc((count_elements(ptr_s, c) + 1) * sizeof(char *));
+	if (!splited_s)
 		return (NULL);
 	i = 0;
+	j = 0;
 	start_i = 0;
-	end_i = 0;
 
 	while (ptr_s[i] != '\0')
 	{
 		if (ptr_s[i] == c)
 		{
-			// *splited_s = malloc((i - last_delim_i) * sizeof(char));
-			*splited_s = ft_substr(ptr_s[start_i], 0, i - start_i);
+			splited_s[j] = ft_substr(ptr_s, start_i, i - start_i);
+			if (!splited_s[j])
+				printf("ERRO");
+			while (ptr_s[i] == c)
+				i++;
 			start_i = i;
+			j++;
 		}
+		else
+			i++;
 	}
 	
 	return (splited_s);
@@ -82,9 +102,14 @@ char	**ft_split(char const *s, char c)
 
 int	main(void)
 {
-	// char	*data[] = {"teste", "abc"};
 	char	s[] = "   teste   abc 123  ";
-	ft_split(s, ' ');
-	// printf("%s\n", data[0]);
-	// printf("%s\n", ft_split(s, ' ')[1]);
+	char	**result;
+
+	result = ft_split(s, ' ');
+	printf("RESULT: %s\n\n", result[2]);
+	while (*result)
+	{
+		printf("RESULT: %s\n\n", *result);
+		result++;
+	}
 }
