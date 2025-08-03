597356546da18451824866b18406ea8732f3854b
Filipe Conde <filipecondep@gmail.com>
2025-08-03 15:56:21 -0300

    Implementa ft_strmapi().

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 14:51:48 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/03 14:52:38 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/03 15:55:28 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-
+	int 	size_s;
+	char	*result;
+	int		i;
+	
+	if (!s || !f)
+		return (NULL);
+	size_s = ft_strlen(s);
+	result = ft_calloc(size_s + 1, sizeof(char));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < size_s)
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	return (result);
 }
