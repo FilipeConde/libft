0068ffcaf14b9151434f344d819ec0e50a612e43
Filipe Conde <filipecondep@gmail.com>
2025-08-03 14:28:11 -0300

    Implementa escrita funcional para números positivos e negativos.

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 19:17:02 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/03 13:46:41 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/03 14:27:36 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,10 @@ static int	ft_numlen(int n)
 char	*ft_itoa(int n)
 {
 	char	*result;
+	int		len;
 
-	result = (char *)ft_calloc(ft_numlen(n) + 1, sizeof(char));
+	len = ft_numlen(n);
+	result = (char *)ft_calloc(len + 1, sizeof(char));
 	if (!result)
 		return (NULL);
 	if (n == 0)
@@ -41,5 +43,23 @@ char	*ft_itoa(int n)
 		*result = '-';
 		n *= -1;
 	}
+	result[len] = '\0';
+	while (len > 0 && n != 0)
+	{
+		len--;
+		result[len] = (n % 10) + '0';
+		n /= 10;
+	}
+
 	return (result);
 }
+// #include <stdio.h>
+
+// int main(void)
+// {
+// 	int num = -2147483648;
+// 	char *str1 = ft_itoa(num);
+// 	printf("%s\n", str1);
+// 	free(str1);
+// 	return (0);
+// }
