8f22f66bb83aeaf113fa5b13e9761b4b94cc43ce
Filipe Conde <filipecondep@gmail.com>
2025-08-03 13:34:02 -0300

    Implementa contador de dígitos.

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,3 +11,28 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	ft_numlen(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*result;
+
+	// decompose int to individual digits
+
+	// handle negative signal
+	return (result);
+}
