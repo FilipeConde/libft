d9e3310aac173154941e779cfea5b59bc0f0390f
Filipe Conde <filipecondep@gmail.com>
2025-08-03 13:48:08 -0300

    Lida com casos n = 0 e escrita de sinal negativo no início da string.

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 19:17:02 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/07/23 22:17:03 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/03 13:46:41 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,15 @@ char	*ft_itoa(int n)
 {
 	char	*result;
 
-	// decompose int to individual digits
-
-	// handle negative signal
+	result = (char *)ft_calloc(ft_numlen(n) + 1, sizeof(char));
+	if (!result)
+		return (NULL);
+	if (n == 0)
+		*result = '0';
+	else if (n < 0)
+	{
+		*result = '-';
+		n *= -1;
+	}
 	return (result);
 }
