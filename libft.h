13361b13ab363464de68bda63a350e8cc9f60173
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-04 23:51:29 -0300

    Integra libft.

--- libft.h
+++ libft.h
@@ -6,11 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:30:31 by fconde-p          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/08/01 20:35:29 by fconde-p         ###   ########.fr       */
-=======
-/*   Updated: 2025/08/04 18:26:37 by fconde-p         ###   ########.fr       */
->>>>>>> main
+/*   Updated: 2025/08/04 23:45:15 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +43,7 @@ void			*ft_calloc(size_t nmemb, size_t size);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
-<<<<<<< HEAD
 char			**ft_split(char const *s, char c);
-=======
 char			*ft_itoa(int n);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void			ft_striteri(char *s, void (*f)(unsigned int, char*));
@@ -57,6 +51,5 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
->>>>>>> main
 
 #endif
