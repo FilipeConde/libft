0b2ddc9aec97f01d42e24786ffcdcf5f6c91cf68
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-05 17:44:07 -0300

    Ajusta função para adequar à norma.

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 17:56:46 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/04 23:45:50 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/05 17:42:51 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,24 +36,27 @@ static int	count_words(char const *s, char c)
 	return (index);
 }
 
-static void	*free_all(char **s, size_t j)
+static int	check_free_all(char **s, size_t j)
 {
-	
-	while (j > 0)
+	if (!*s[j])
 	{
-		free(s[j - 0]);
-		j--;
+		while (j > 0)
+		{
+			free(s[j - 0]);
+			j--;
+		}
+		free(s);
+		return (0);
 	}
-	free(s);
-	return (NULL);
+	return (1);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char			**splited_s;
-	size_t			start_i;
-	size_t			i;
-	int				j;
+	char	**splited_s;
+	size_t	start_i;
+	size_t	i;
+	int		j;
 
 	splited_s = ft_calloc((count_words(s, c) + 1), sizeof(char *));
 	if (!splited_s)
@@ -63,42 +66,16 @@ char	**ft_split(char const *s, char c)
 	start_i = 0;
 	while (s[i] != '\0' && j < count_words(s, c))
 	{
-			// start_i = i;
-			while (s[i] == c && s[i] != '\0')
-				i++;
-			start_i = i;
-			while (s[i] != c && s[i] != '\0')
-				i++;
-			splited_s[j] = ft_substr(s, start_i, i - start_i);
-			if (!splited_s[j])
-			{
-				free_all(splited_s, j);
-				return (NULL);
-			}
-			j++;
-		// while (s[i] == c)
-		// 	i++;
+		while (s[i] == c && s[i] != '\0')
+			i++;
+		start_i = i;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		splited_s[j] = ft_substr(s, start_i, i - start_i);
+		if (!check_free_all(splited_s, j))
+			return (NULL);
+		j++;
 	}
 	splited_s[j] = NULL;
 	return (splited_s);
 }
-
-// int	main(void)
-// {
-// 	char	s[] = "--teste--abc--123--";
-// 	char	**result;
-
-// 	result = ft_split(s, '-');
-// 	// printf("RESULT: %s\n\n", result[2]);
-// 	if (!result)
-// 	{
-// 		free(result);
-// 		return (0);
-// 	}
-// 	while (*result)
-// 	{
-// 		printf("RESULT: %s\n\n", *result);
-// 		result++;
-// 	}
-// 	return (0);
-// }
