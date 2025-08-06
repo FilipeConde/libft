1d2f0f617035ce00a791194e94be51a413dea8a8
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-06 19:50:09 -0300

    Estabelece início de ft_lstnew().

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:30:31 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/06 19:23:33 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/06 19:47:37 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,5 +57,7 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
+
+t_list			*ft_lstnew(void *content);
 
 #endif
