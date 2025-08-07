022b91f39d9769901fe127f6fd658bb50e8e485a
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-06 23:03:19 -0300

    Implementa ft_strmap().

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 22:25:49 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/06 22:28:06 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/06 22:59:45 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new_node;
+	t_list	*mapped_lst;
 	
+	if (!lst || !f || !del)
+		return (NULL);
+	mapped_lst = NULL;
+	while (lst != NULL)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		ft_lstadd_back(&mapped_lst, new_node);
+		if (!mapped_lst)
+		{
+			ft_lstclear(&mapped_lst, del);
+		}
+		lst = lst->next;
+	}
+	return (mapped_lst);
 }
