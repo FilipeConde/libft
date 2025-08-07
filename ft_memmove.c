5983011e9bed65b56d228e87ff125432ea5ea292
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-07 18:28:35 -0300

    Ajust norma.

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,17 +6,17 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 19:55:22 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/07 18:15:57 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/07 18:22:36 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t i;
-	const char *s;
-	char *d;
+	size_t		i;
+	const char	*s;
+	char		*d;
 
 	s = src;
 	d = dest;
